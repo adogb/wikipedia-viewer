38037840016f95c67c1d095f00baaa63fb1a5638
arya45 <audrey.dogbeh@gmail.com>
2017-01-10 23:03:18 +0000

    Clear results list for new search

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     /*Initialise variables*/
     var topic, userUrl, $resultsListItems, title, snippet;
+    $resultsListItems = $("#resultsList li");
 
     /* Event handler when the search button is clicked*/
     $("#searchBar button").click(function(){
@@ -18,16 +19,26 @@ $(document).ready(function(){
     function getData(){
         topic = $('#topicInput').val();
         userUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="+topic+"&srlimit=10&srprop=snippet&callback=?";
-        $resultsListItems = $("#resultsList").find("li");
         console.log(userUrl);
         $.getJSON(userUrl, function(data) {
-            $resultsListItems.each(function(index){
-                title = data.query.search[index].title;
-                $(this).find("a").attr("href","https://en.wikipedia.org/wiki/"+encodeURIComponent(title));
-                $(this).find("h1").text(title);
-                $(this).find("p").html(data.query.search[index].snippet);
-            });
-
+            if (data.query.searchinfo.totalhits==0){
+                $resultsListItems.each(function(index){
+                    $(this).find("a").attr("href","");
+                    $(this).find("h1").text("");
+                    $(this).find("p").html("");
+                });
+                $("#error").find("h1").text("Hmm...");
+                $("#error").find("p").text("Sorry, no search results were found. Wikipedia knows a lot but not "+topic+"... Did you make a typo?");
+                
+            } else {
+                 console.log(typeof $resultsListItems[0]);
+                 $resultsListItems.each(function(index){                    
+                    title = data.query.search[index].title;
+                    $(this).find("a").attr("href","https://en.wikipedia.org/wiki/"+encodeURIComponent(title));
+                    $(this).find("h1").text(title);
+                    $(this).find("p").html(data.query.search[index].snippet);
+                });
+            } 
         });
     }
 });
