ec1bbd46e36fdae1cd306dfdf58e48226c486ef4
arya45 <audrey.dogbeh@gmail.com>
2017-01-11 23:07:14 +0000

    add hide show for error div

--- js/script.js
+++ js/script.js
@@ -19,7 +19,6 @@ $(document).ready(function(){
     function getData(){
         topic = $('#topicInput').val();
         userUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="+topic+"&srlimit=10&srprop=snippet&callback=?";
-        console.log(userUrl);
         $.getJSON(userUrl, function(data) {
             if (data.query.searchinfo.totalhits==0){
                 $resultsListItems.each(function(index){
@@ -27,11 +26,12 @@ $(document).ready(function(){
                     $(this).find("h1").text("");
                     $(this).find("p").html("");
                 });
+                $("#error").show();
                 $("#error").find("h1").text("Hmm...");
                 $("#error").find("p").text("Sorry, no search results were found. Wikipedia knows a lot but not "+topic+"... Did you make a typo?");
                 
             } else {
-                 console.log(typeof $resultsListItems[0]);
+                 $("#error").hide();
                  $resultsListItems.each(function(index){                    
                     title = data.query.search[index].title;
                     $(this).find("a").attr("href","https://en.wikipedia.org/wiki/"+encodeURIComponent(title));
